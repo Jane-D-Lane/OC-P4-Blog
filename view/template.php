3663f460faa7dbe2e64806fe166791910dd57bbc
Jane Lane <gwenaelle.poirier@ntymail.com>
2020-09-02 09:05:04 +0200

    1ere mise en page

--- view/template.php
+++ view/template.php
@@ -3,10 +3,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" type="text/css" href="blog.css">
+		<link rel="stylesheet" type="text/css" href="public/blog.css">
 		<title><?= $title ?></title>
 	</head>
 	<body>
+		<header>
+			<h1><?= $titlePage ?></h1>
+		</header>
 		<?= $content ?>
 	</body>
 </html>
